17613b90e8420a436c43bfd708a5c9e22c3642a9
bd-dev-ismail <ismailratul7@gmail.com>
2022-09-20 15:51:49 +0600

    added\

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+      <ExtranlUsers></ExtranlUsers>
     </div>
   );
+}
+function ExtranlUsers(){
+  const [users, setUsers] = useState([]);
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, []);
+  return (
+    <div>
+      <h2>Extranl Users</h2>
+      <p>{users.length}</p>
+      {
+        users.map((user) => <User name={user.name} email={user.email}></User> )
+      }
+    </div>
+  );
+}
+function User(props){
+  return (
+    <div style={{border: '2px solid red', margin: '20px', }}>
+      <h3>Name: {props.name}</h3>
+      <p>Email: {props.email}</p>
+    </div>
+  )
 }
 function Counter(){
   //Using State - something want to change
